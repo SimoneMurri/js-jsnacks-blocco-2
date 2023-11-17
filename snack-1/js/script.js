046b32bcf73ae5270ac1b7bf6c514ae6d2cef2cd
Simone Murri <simonemurri701@gmail.com>
2023-11-17 11:28:50 +0100

    creato array vuoto e chiesto all'utente un numero

---
+++ snack-1/js/script.js
@@ -0,0 +1,8 @@
+//Crea un array vuoto e chiedi all'utente un numero da inserire nell'array. Continua a chiedere i numeri all'utente e a inserirli nell'array fino a quando la somma degli elementi è minore di 50.//
+
+
+const array = [];
+
+let userNumber = prompt("Type your number");
+array.push(userNumber);
+
