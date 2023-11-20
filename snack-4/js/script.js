5f07e77227197d23660e0885e89dd9dd9dfde01d
Simone Murri <simonemurri701@gmail.com>
2023-11-20 12:18:38 +0100

    snack-4 inserito solo primo numero divisibile true

---
+++ snack-4/js/script.js
@@ -0,0 +1,11 @@
+
+function divisibleNumbers() {
+    const numberOne = parseInt(prompt("Typer your number"));
+    const numberTwo = parseInt(prompt("Type your number"));
+}
+
+const divisible = divisibleNumbers();
+
+if (divisible !== false) {
+    console.log("Il primo numero è divisibile per il secondo");
+} 
